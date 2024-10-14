e82c3cfc8dac797f0f3eeb6068f8c7c010c39a49
Gergo Buglyo <buglyogergo@gmail.com>
2024-10-14 23:41:08 +0200

    Created 5 task and implement pipes

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,18 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'duration'
+  name: "duration",
 })
-export class DurationPipe {
-    // Add your code here
+export class DurationPipe implements PipeTransform {
+  transform(duration: number): string {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+
+    const formattedHours = hours < 10 ? "0" + hours : hours;
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+
+    return `${formattedHours}:${formattedMinutes} hour${
+      hours === 1 ? "" : "s"
+    }`;
+  }
 }
