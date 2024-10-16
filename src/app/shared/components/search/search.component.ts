3bd76b6d728cbdd6075e1cc877c6e40b82a63af3
Gergo Buglyo <buglyogergo@gmail.com>
2024-10-16 14:20:39 +0200

    Creataed 6 task

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.scss"],
 })
 export class SearchComponent {
-  // Use the name `placeholder` for the @Input.
-  // Use the name `search` for the @Output.
-}
+  @Input() placeholder = "Input Text";
+  @Output() search = new EventEmitter<string>();
 
+  searchText = "";
+
+  onSubmit() {
+    if (this.searchText.length >= 2) {
+      this.search.emit(this.searchText);
+      console.log(this.searchText);
+    }
+  }
+}
